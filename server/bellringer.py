63933ccbfeb1e4faff5ca829b6472f44ddb51727
BuildTools <jmeredit@ashland.edu>
2018-03-14 17:13:02 -0400

    Update

--- server/bellringer.py
+++ server/bellringer.py
@@ -8,8 +8,9 @@ def bellringer(sound_type) :
 
 	temp_volume = "100 dB"
 	os.system("amixer cset numid=3 1")
-	os.system("amixer -q -- sset PPCM playback " + temp_volume)
-	os.system("aplay -q -D sysdefault /bellsound.wav")
+	#os.system("amixer -q -- sset PPCM playback " + temp_volume)
+	os.system("amixer set PCM -- -100.00")
+	os.system("aplay -q -D sysdefault bellsound.wav")
 
 bellringer(1)
 
