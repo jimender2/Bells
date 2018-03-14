944c2385afe2b1d5d45ca70ec7ad6908ba96c06e
BuildTools <jmeredit@ashland.edu>
2018-03-14 17:16:19 -0400

    Direct path

--- server/bellringer.py
+++ server/bellringer.py
@@ -9,8 +9,8 @@ def bellringer(sound_type) :
 	temp_volume = "100 dB"
 	os.system("amixer cset numid=3 1")
 	#os.system("amixer -q -- sset PPCM playback " + temp_volume)
-	os.system("amixer set PCM -- -100.00")
-	os.system("aplay -q -D sysdefault bellsound.wav")
+	os.system("amixer sset PCM,0 100%")
+	os.system("aplay -q -D sysdefault /home/pi/Bells/server/bellsound.wav")
 
 bellringer(1)
 
