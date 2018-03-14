351985b71181a99aa0ab08dcc5040b98bc6e3276
BuildTools <jmeredit@ashland.edu>
2018-03-14 16:47:23 -0400

    Force output?

--- server/bellringer.py
+++ server/bellringer.py
@@ -7,5 +7,6 @@ import wave
 def bellringer(sound_type) :
 
 	temp_volume = 100 + "dB"
+	os.system("amixer cset numid=3 1")
 	os.system("amixer -q -- sset PPCM playback " + temp_volume)
 	os.system("aplay -q -D sysdefault bellringer.wav")
