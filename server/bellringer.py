be3d4b5ce56ad15eddc8d29c58d45cda82d68a61
BuildTools <jmeredit@ashland.edu>
2018-03-14 16:42:49 -0400

    Update

--- server/bellringer.py
+++ server/bellringer.py
@@ -4,41 +4,8 @@
 import pyaudio  
 import wave  
 
-<<<<<<< HEAD
 def bellringer(sound_type) :
-<<<<<<< HEAD
-	os.system("amixer -q -- sset PPCM playback 100 dB")
-	os.system("aplay -q -D sysdefault bellringer.wav")
-=======
-#define stream chunk   
-chunk = 1024  
 
-#open a wav format music  
-f = wave.open("bellsound.wav","rb")  
-#instantiate PyAudio  
-p = pyaudio.PyAudio()  
-#open stream  
-stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-                channels = f.getnchannels(),  
-                rate = f.getframerate(),  
-                output = True)  
-#read data  
-data = f.readframes(chunk)  
-
-#play stream  
-while data:  
-    stream.write(data)  
-    data = f.readframes(chunk)  
-
-#stop stream  
-stream.stop_stream()  
-stream.close()  
-
-#close PyAudio  
-p.terminate()
->>>>>>> parent of 5200dd8... Use aplayer
-=======
 	temp_volume = 100 + "dB"
 	os.system("amixer -q -- sset PPCM playback " + temp_volume)
 	os.system("aplay -q -D sysdefault bellringer.wav")
->>>>>>> parent of e79e9d5... Update sound
