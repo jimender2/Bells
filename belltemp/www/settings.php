8210f3f5ad0e1d9d5b7ae05589a6a6323cebb5b8
BuildTools <jmeredit@ashland.edu>
2018-03-25 08:34:24 -0400

    Update Some language

--- belltemp/www/settings.php
+++ belltemp/www/settings.php
@@ -1,33 +1,4 @@
 <?php
-/*
-*      settings.php
-*
-*      This file if part of the "sophia" bell management system
-*      For more information on the software please visit:
-*      https://lizzit.it/sophia
-*
-*      Written by: Michele Lizzit <michele@lizzit.it>, 20 Mar 2014
-*      Last update: 22 Sept 2017
-*      Version: 1.2
-*
-*      Copyright (c) 2016 Michele Lizzit
-*
-*      This program is free software: you can redistribute it and/or modify
-*      it under the terms of the GNU Affero General Public License as published
-*      by the Free Software Foundation, either version 3 of the License, or
-*      (at your option) any later version.
-*
-*      This program is distributed in the hope that it will be useful,
-*      but WITHOUT ANY WARRANTY; without even the implied warranty of
-*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*      GNU Affero General Public License for more details.
-*
-*      You should have received a copy of the GNU Affero General Public License
-*      along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-?>
-
-<?php
 require('definitions.php');
 
 $error = 0;
@@ -59,7 +30,7 @@ if (isset($_POST['enable']) && isset($_POST['orari']) && isset($_POST['ntp_updat
 		$update_ntp_now = 0;
 	}
 
-	$options_string = "#le righe che iniziano con # vengono scartate\n"
+	$options_string = "#lines starting with # are discarded\n"
 	. $lines .
 	"\n#enable\n"
 	. $_POST['enable'] .
@@ -73,9 +44,9 @@ if (isset($_POST['enable']) && isset($_POST['orari']) && isset($_POST['ntp_updat
 	. $_POST['ntp_update_time'] .
 	"\n#update ntp now\n"
 	. $update_ntp_now .
-	"\n#NON MODIFICARE QUESTO COMMENTO squilla ora\n"
+	"\n#DO NOT MODIFY THIS COMMENT rings now\n"
 	. $squilla_ora .
-	"\n#num_options includendo questa\n" .
+	"\n#num_options including this\n" .
 	"9\n"
 	. $timetable;
 
@@ -126,7 +97,7 @@ textarea {
 			<div id="main" style="width:540px;"> <p> <span id="title" class="settingsTitle"></span></p>
 				<?php
 				if ($error == 1) {
-					echo '		<script language="javascript" type="text/javascript"> alert("Hai compilato male il form")</script>';
+					echo '		<script language="javascript" type="text/javascript"> alert("You have filled out the form")</script>';
 				}
 				?>
 
