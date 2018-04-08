26b478e8655f337ba1113059b89fb7db4cf98362
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-04-08 18:46:02 -0400

    Update init_sophia.sh

--- bellupdate/installer_files/init_sophia.sh
+++ bellupdate/installer_files/init_sophia.sh
@@ -12,32 +12,6 @@
 # Description:       sophia init script
 ### END INIT INFO
 
-###
- #      init_sophia.sh
- #
- #      Init script of "sophia" software.
- #		For more information on the software please visit:
- #		https://lizzit.it/sophia
- #
- #      Written by: Michele Lizzit <michele@lizzit.it>, 20 Mar 2014
- #      Last update: 25 Apr 2016
- #      Version: 1.0
- #
- #      Copyright (c) 2016 Michele Lizzit
- #      
- #      This program is free software: you can redistribute it and/or modify
- #      it under the terms of the GNU Affero General Public License as published
- #      by the Free Software Foundation, either version 3 of the License, or
- #      (at your option) any later version.
- #    
- #      This program is distributed in the hope that it will be useful,
- #      but WITHOUT ANY WARRANTY; without even the implied warranty of
- #      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- #      GNU Affero General Public License for more details.
- #    
- #      You should have received a copy of the GNU Affero General Public License
- #      along with this program.  If not, see <http://www.gnu.org/licenses/>.
-###
 
 case "$1" in
 	start)
