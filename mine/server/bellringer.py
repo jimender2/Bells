1e1bf02f868726d62372ccef3c22792e03feecf0
BuildTools <jmeredit@ashland.edu>
2018-04-15 17:37:25 -0400

    Got part of mine working

--- mine/server/bellringer.py
+++ mine/server/bellringer.py
@@ -2,26 +2,43 @@
 #coding=utf-8  
 
 import time
+import datetime
 import os
+import winsound
+
 
 def bellringer(sound_type) :
 
 	temp_volume = "100 dB"
-	os.system("amixer cset numid=3 1")
-	#os.system("amixer -q -- sset PPCM playback " + temp_volume)
-	os.system("amixer sset PCM,0 100%")
-	os.system("aplay -q -D sysdefault /home/pi/Bells/server/bellsound.wav")
+	
+	#for windows
+	winsound.PlaySound('bellsound.wav', winsound.SND_FILENAME)
+
+	#for rasparian
+	#os.system("amixer cset numid=3 1")
+	#os.system("amixer sset PCM,0 100%")
+	#os.system("aplay -q -D sysdefault /home/pi/Bells/server/bellsound.wav")
 
 optionRead = open("options.txt", "r")
-bellOne = optionRead.readline()
-bellTwo = optionRead.readline()
-bellThree = optionRead.readline()
-bellFour = optionRead.readline()
+day = optionRead.readline()
+day = day.rstrip('\n')
+dayandtime = day + ' ' + optionRead.readline()
+dayandtime = dayandtime.rstrip('\n')
+
+
+bellOne = datetime.datetime.strptime(dayandtime, '%m/%d/%Y %H:%M:%S')
+
+
+#bellTwo = optionRead.readline()
+#bellThree = optionRead.readline()
+#bellFour = optionRead.readline()
 
 print(bellOne)
-print(bellTwo)
-print(bellThree)
-print(bellFour)
 
-bellringer(1)
-
+while 1==1:
+	while datetime.datetime.now() < bellOne:
+		time.sleep(1)
+	bellringer(1)
+	dayandtime = day + ' ' + optionRead.readline()
+	dayandtime = dayandtime.rstrip('\n')
+	bellOne = datetime.datetime.strptime(dayandtime, '%m/%d/%Y %H:%M:%S')
