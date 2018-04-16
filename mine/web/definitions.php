c821053e7def699841f3484af9faa18a850ddcaf
BuildTools <jmeredit@ashland.edu>
2018-04-15 21:10:58 -0400

    fixed definitions file

--- mine/web/definitions.php
+++ mine/web/definitions.php
@@ -1,6 +1,6 @@
 <?php
 $SOUNDFILE_UPLOAD_DIR = "/var/www/uploads/";
-$ACTIVE_SOUNDFILE_PATH = "/opt/sophia/data/bellsound.wav";
+$ACTIVE_SOUNDFILE_PATH = "/opt/sophia/bellsound.wav";
 $OPTIONS_FILE_PATH = "/opt/sophia/OPTIONS.txt";
 $RESTART_sophia_SCRIPT_PATH = "/opt/sophia/restart_script.sh";
 ?>
