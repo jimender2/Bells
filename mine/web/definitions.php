c28c62aac925fc8fca2840e6ff4844ffe23a9823
BuildTools <jmeredit@ashland.edu>
2018-04-15 22:06:00 -0400

    Made the update settings page actually work with my options

--- mine/web/definitions.php
+++ mine/web/definitions.php
@@ -1,6 +1,6 @@
 <?php
 $SOUNDFILE_UPLOAD_DIR = "/var/www/uploads/";
 $ACTIVE_SOUNDFILE_PATH = "/opt/sophia/bellsound.wav";
-$OPTIONS_FILE_PATH = "/opt/sophia/OPTIONS.txt";
+$OPTIONS_FILE_PATH = "/opt/sophia/options.txt";
 $RESTART_sophia_SCRIPT_PATH = "/opt/sophia/restart_script.sh";
 ?>
