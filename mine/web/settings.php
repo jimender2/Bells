33245012e248953d78d51a14c89a2a1a06a940ec
BuildTools <jmeredit@ashland.edu>
2018-04-15 22:12:36 -0400

    Fixed path for options file

--- mine/web/settings.php
+++ mine/web/settings.php
@@ -1,19 +1,19 @@
 <?php
 if($_POST['Submit']){ 
-$open = fopen("/opt/sophia/OPTIONS.txt","w+"); 
+$open = fopen("/opt/sophia/options.txt","w+"); 
 $text = $_POST['update']; 
 fwrite($open, $text); 
 fclose($open); 
 echo "File updated.<br />";  
 echo "File:<br />"; 
-$file = file("options.txt"); 
+$file = file("/opt/sophia/options.txt"); 
 foreach($file as $text) { 
 echo $text."<br />"; 
 } 
 }else{ 
 $file = file("options.txt"); 
 echo "<form action=\"".$PHP_SELF."\" method=\"post\">"; 
-echo "<textarea Name=\"update\" cols=\"50\" rows=\"10\">"; 
+echo "<textarea Name=\"update\" cols=\"50\" rows=\"20\">"; 
 foreach($file as $text) { 
 echo $text; 
 }  
