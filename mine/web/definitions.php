d9952f7a27f653acc2c010a04ec24a3dfdc80956
BuildTools <jmeredit@ashland.edu>
2018-04-15 21:10:31 -0400

    had to add definitions file so we can find stuff

---
+++ mine/web/definitions.php
@@ -0,0 +1,6 @@
+<?php
+$SOUNDFILE_UPLOAD_DIR = "/var/www/uploads/";
+$ACTIVE_SOUNDFILE_PATH = "/opt/sophia/data/bellsound.wav";
+$OPTIONS_FILE_PATH = "/opt/sophia/OPTIONS.txt";
+$RESTART_sophia_SCRIPT_PATH = "/opt/sophia/restart_script.sh";
+?>
