3e920898348448aae2f2cdac7d5cadc15d72a7ae
BuildTools <jmeredit@ashland.edu>
2018-05-01 06:33:39 -0400

    Got rid of belltemp

--- belltemp/www/definitions.php
+++
@@ -1,6 +0,0 @@
-<?php
-$SOUNDFILE_UPLOAD_DIR = "/var/www/uploads/";
-$ACTIVE_SOUNDFILE_PATH = "/opt/sophia/data/suono.wav";
-$OPTIONS_FILE_PATH = "/opt/sophia/OPTIONS.txt";
-$RESTART_sophia_SCRIPT_PATH = "/opt/sophia/restart_script.sh";
-?>
