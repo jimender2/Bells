216a4e425ac17f35cf0d5eed95a285a3f753af80
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-06-26 22:31:55 -0400

    Update bellringer.py

--- mine/server/bellringer.py
+++ mine/server/bellringer.py
@@ -34,7 +34,72 @@ def bellringer(sound_type) :
 	#for rasparian
 	os.system("amixer cset numid=3 1")
 	os.system("amixer sset PCM,0 100%")
-	os.system("aplay -q -D sysdefault /home/pi/Bells/server/bellsound.wav")
+	os.system("aplay -q -D sysdefault /opt/sophia/bellsound.wav")
+
+def defaultDay():
+	defaultFile = open("default.txt", "r")
+	#Set some variables
+	donotend = True
+	oneminold = datetime.timedelta(days=0, hours=0, minutes=1, seconds=0)
+	
+	#get todays date
+	day = datetime.datetime.now().strftime("%m/%d/%Y")
+
+	#read time from file
+	dayandtime = day + ' ' + defaultFile.readline()
+	#strip formatting from the string
+	dayandtime = dayandtime.rstrip('\n')
+	dayandtime = dayandtime.rstrip('\r')
+	dayandtime = dayandtime.rstrip()
+
+	bell = datetime.datetime.strptime(dayandtime, '%m/%d/%Y %H:%M:%S')
+	while donotend==True:
+		f.write(dayandtime)
+		f.write('\n')
+		print(bell)
+		
+		#saves cpu cycles
+		while datetime.datetime.now() < bell:
+			time.sleep(1)
+			
+		#make it not ring if more than a minute old
+		timeElapsed = datetime.datetime.now() - bell
+			
+		f.write(str(timeElapsed))
+		f.write('\n')
+		print(timeElapsed)
+			
+		while 1==1:
+			if timeElapsed > oneminold:
+				break
+			bellringer(1)
+			break
+				
+		timefromfile = defaultFile.readline()
+		timefromfile = timefromfile.rstrip('\n')
+		timefromfile = timefromfile.rstrip('\r')
+		timefromfile = timefromfile.rstrip()
+
+		if timefromfile == "done":
+			break
+
+		dayandtime = day + ' ' + timefromfile
+		bell = datetime.datetime.strptime(dayandtime, '%m/%d/%Y %H:%M:%S')
+			
+		#write everything from buffer to file
+		f.flush()
+		os.fsync(f.fileno())
+			
+		print("done with default " + day)
+		f.write("done with default " + day)
+		f.write('\n')
+		
+		#write everything from buffer to file
+		f.flush()
+		os.fsync(f.fileno())
+		##END OF FUNCTION
+		##END OF FUNCTION
+
 
 #Open the options file.
 optionRead = open("options.txt", "r")
@@ -43,6 +108,8 @@ optionRead = open("options.txt", "r")
 moretofile = True
 donotend = True
 oneminold = datetime.timedelta(days=0, hours=0, minutes=1, seconds=0)
+
+onedayold = datetime.timedelta(days=1, hours=0, minutes=0, seconds=0)
 
 #Day run
 while moretofile == True:
@@ -65,6 +132,13 @@ while moretofile == True:
 		f.write(dayandtime)
 		f.write('\n')
 		print(bell)
+		
+		if ( (bell - datetime.datetime.now()) > onedayold ):
+			f.write("went into the function")
+			f.flush()
+			os.fsync(f.fileno())
+			defaultDay()
+		
 		#saves cpu cycles
 		while datetime.datetime.now() < bell:
 			time.sleep(1)
